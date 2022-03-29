e8a0d39faf0fb002f112a4c9111d9bfc793a6a7f
sam kelly <samkelly1290@gmail.com>
2022-03-29 20:59:52 +1100

    its fucked

--- ymirdb.c
+++ ymirdb.c
@@ -635,8 +635,8 @@ void recurse_backward(entry* current_entry, entry* end, snapshot* snapshots, int
 	}
 	for (int back_index = current_entry->backward_size - 1; back_index >= 0; back_index--) {
 		entry* back_entry = get_entry(current_entry->backward[back_index].key, snapshots, snapshot_number);
-		recurse_backward(back_entry, end, snapshots, snapshot_number);
 		printf("%s", current_entry->backward[back_index].key);
+		recurse_backward(back_entry, end, snapshots, snapshot_number);
 		if (!(back_index == 0 && strcmp(current_entry->key, end->key) == 0)) {
 			printf(", ");
 		}
