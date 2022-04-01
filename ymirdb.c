4a096ba8d7104a880317d891a04d8d221414cecc
sam kelly <samkelly1290@gmail.com>
2022-04-01 21:28:27 +1100

    what

--- ymirdb.c
+++ ymirdb.c
@@ -46,7 +46,7 @@ void command_bye(snapshot* snapshots) {
 		free(current_state.entries[current_entry].values);
 	}
 	free(current_state.entries);
-	for (int current_snapshot = 0; current_snapshot < snapshot_counter; current_snapshot++) {
+	for (int current_snapshot = 0; current_snapshot < total_snapshots; current_snapshot++) {
 		current_state = snapshots[current_snapshot];
 		for (int current_entry = 0; current_entry < snapshots[current_snapshot].num_entries; current_entry++) {
 			entry* free_entry = get_entry(snapshots[current_snapshot].entries[current_entry].key);
